15bdbe18b09e2ba435cd50c651ce0cdc5d585a88
JaredWorrick <115746414+JaredWorrick@users.noreply.github.com>
2023-02-09 22:34:22 -0600

    Adding files to repo
    Files for JavaScript Challenge

---
+++ app.js
@@ -0,0 +1,51 @@
+// Display the default plot
+function init() {
+    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(function (data) {
+      console.log(data);
+         names = data.samples.map(x=>x.id)
+         names.forEach(function(name) {
+            d3.select('#selDataset').append('option').text(name)
+            });
+    
+    var sample_values = data.samples.map(x=> x.sample_values);
+    var otu_ids = data.samples.map(x=> x.otu_ids);
+    var otu_label = data.samples.map(x=> x.otu_labels);
+    
+    // Create Bar Chart
+    var trace1 = {
+        x : top_ten[0],
+        y : top_ids[0].map(x => "OTU" + x),
+        text : top_labels[0],
+        type : 'bar',
+        orientation : 'h',
+        transforms: [{
+            type: 'sort',
+            target: 'y',
+            order: 'descending',
+          }],
+    };
+
+  
+    // Draw the bar chart
+    var data = [trace1];
+    var config = {responsive:true}
+    Plotly.newPlot('bar', data,config);
+  
+  
+    // Create a bubble chart 
+    var trace2 = {
+        x : otu_ids[0],
+        y : sample_values[0],
+        text : otu_label[0],
+        mode : 'markers',
+        marker : {
+            color : otu_ids[0],
+            size : sample_values[0]
+        }
+    };
+  
+    // Draw the bubble chart
+    var data2 = [trace2];
+    var config = {responsive:true}
+    Plotly.newPlot('bubble',data2,config);
+init();
